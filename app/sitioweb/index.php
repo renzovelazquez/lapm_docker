051ef8d9b8f9a4c240f46275f163101a4ba9a675
Rezo Velazquez <renzovelazquez@gmail.com>
2021-08-05 00:32:06 -0300

    agremamos sitiobeb

---
+++ app/sitioweb/index.php
@@ -0,0 +1,57 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Sitio Web</title>
+    <link rel="stylesheet" href="./css/bootstrap.min.css" />
+
+</head
+<body>
+itio Webs
+     <nav class="navbar navbar-expand navbar-dark bg-dark">
+         <ul class="nav navbar-nav">
+             
+            <li class="nav-item active">
+                 <a class="nav-link" href="#">Dveloteca</a>
+             </li>
+            
+             <li class="nav-item active">
+                 <a class="nav-link" href="#">Inicio</a>
+             </li>
+            
+             <li class="nav-item">
+                 <a class="nav-link" href="#">Libros</a>
+             </li>
+
+             <li class="nav-item active">
+                 <a class="nav-link" href="#">Nosotros</a>
+             </li>
+         </ul>
+     </nav>
+     
+     <div class="container">
+         <div class="row">
+            
+            <div class="jumbotron">
+                <h1 class="display-3">Jumbo heading</h1>
+                <p class="lead">Jumbo helper text</p>
+                <hr class="my-2">
+                <p>More info</p>
+                <p class="lead">
+                    <a class="btn btn-primary btn-lg" href="Jumbo action link" role="button">Jumbo action name</a>
+                </p>
+            </div>       
+
+
+
+
+
+         </div>
+     </div>
+
+
+</body>
+    
+</html>
