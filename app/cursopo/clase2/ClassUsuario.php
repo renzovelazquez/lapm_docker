59a18c8765d266a9d49b4ebcc98879fdeee0b4c9
Renzo Velazquez <renzovelazquez@gmail.com>
2023-11-29 06:46:28 -0300

    Implementacion de Getters y Setters

--- app/cursopo/clase2/ClassUsuario.php
+++ app/cursopo/clase2/ClassUsuario.php
@@ -1,8 +1,8 @@
 <?php
     Class Usuario{
-        public $strNombre;
-        public $strEmail;
-        public $strTipo;
+        private $strNombre;
+        private $strEmail;
+        private $strTipo;
         private $strClave;
         protected $strFechaRegitro;
         static $strEstado = "Activo";
@@ -12,7 +12,43 @@
             $this->strNombre = $nombre;
             $this->strEmail = $email;
             $this->strTipo = $tipo;
-            $this->strClve = rand();
+            $this->strClave = rand();
+            $this->strFechaRegitro = date('y-m-d H:m:s');
+        }
+
+        // para acceder a las propiedades tanto protected  como private en este caso tenesmo que hacer 
+        // uso de métodos geter y seter
+        
+        //defino los geters para acceder a las propiedades privadas
+        public function getNombre():string
+        {
+            return $this->strNombre;
+        }
+
+        public function getEmail():string
+        {
+            return $this->strEmail;
+        }
+
+        public function getTipo():string
+        {
+            return $this->strTipo;
+        }
+
+        // crar un metodo que muestre todos los datos del Usuario
+        public function getPerfil()
+        {
+            echo "Datos del usuario <br>";
+            echo "Nombre: ". $this->strNombre."<br>";
+            echo "Email: ". $this->strEmail."<br>";
+            echo "clave: ". $this->strClave."<br>";
+            echo "Fecha registro: ". $this->strFechaRegitro."<br>";
+            echo "Estado: ". self::$strEstado."<br>";
+        }
+        // seter se puede sear el valor de una propidedad
+        public function setCambiarClave(string $pass)
+        {
+            $this->strClave = $pass;
         }
 
     }//End Class usuario
--- app/cursopo/clase2/usuario.php
+++ app/cursopo/clase2/usuario.php
@@ -1,8 +1,25 @@
 <?php
     require_once("ClassUsuario.php");
+    
     $objetousuario = new Usuario("Andres cardona", "andres@info.com", "Admin");
+    $objandre = new Usuario("Andrea Arana", "andrea@info.com", "Cliente");
+    //atributos de tipo publico 
+    //echo $objetousuario->getNombre();
+    //echo "<br>";
 
-    echo Usuario::$strEstado;
+    //echo $objetousuario->getTipo();
+    //echo "<br>";
+    // private no se puede accder desde 
+    //echo $objetousuario->strClave;
+    //echo "<br>";
+    // static mas bien pertenece a la clase, por eso no es necesario instanciasr la clase
+    //echo Usuario::$strEstado;
+    echo $objetousuario->getPerfil();
+    echo "<br> <br>";
+    echo $objandre->getPerfil(); 
+    //hago el cabmio de clave
+    $objandre->setCambiarClave('123456789');
 
-
+    echo "<br> <br>";
+    echo $objandre->getPerfil(); 
 ?>
